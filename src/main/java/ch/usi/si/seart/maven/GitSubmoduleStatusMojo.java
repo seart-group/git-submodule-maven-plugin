78dbace557019eadded2c12db900530661c7cb9c
dabico <dabico@usi.ch>
2024-07-24 15:02:55 +0200

    Preliminary implementation of `GitSubmoduleStatusMojo`

---
+++ src/main/java/ch/usi/si/seart/maven/GitSubmoduleStatusMojo.java
@@ -0,0 +1,28 @@
+package ch.usi.si.seart.maven;
+
+import org.apache.maven.plugins.annotations.Mojo;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.submodule.SubmoduleStatus;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+@Mojo(name = "status")
+public class GitSubmoduleStatusMojo extends GitSubmoduleMojo {
+
+    @Override
+    protected void execute(Git git) throws Exception {
+        Repository repository = git.getRepository();
+        Path root = repository.getWorkTree().toPath();
+        Map<String, SubmoduleStatus> statuses = git.submoduleStatus().call();
+        for (SubmoduleStatus status : statuses.values()) {
+            Path absolute = Paths.get(status.getPath());
+            Path relative = root.relativize(absolute);
+            String sha = status.getHeadId().getName();
+            String message = String.format("%s\t%s", sha, relative);
+            getLog().info(message);
+        }
+    }
+}
