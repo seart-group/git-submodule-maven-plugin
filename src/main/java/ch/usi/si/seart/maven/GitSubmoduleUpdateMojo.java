513ccf7d9d2c9161f810e32055f7681ca715fa4a
dabico <dabico@usi.ch>
2024-07-24 14:19:07 +0200

    Make use of new abstraction in `GitSubmoduleUpdateMojo`

--- src/main/java/ch/usi/si/seart/maven/GitSubmoduleUpdateMojo.java
+++ src/main/java/ch/usi/si/seart/maven/GitSubmoduleUpdateMojo.java
@@ -1,16 +1,12 @@
 package ch.usi.si.seart.maven;
 
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.submodule.SubmoduleWalk;
 
-import java.io.File;
 import java.io.IOException;
 
 /**
@@ -22,58 +18,14 @@ import java.io.IOException;
  * @author Ozren Dabić
  */
 @Mojo(name = "update", defaultPhase = LifecyclePhase.INITIALIZE)
-public class GitSubmoduleUpdateMojo extends AbstractMojo {
-
-    /**
-     * When set to {@code true} the plugin execution will completely skip. This is useful for profile-activated plugin
-     * invocations or to use properties to enable/disable pom features.
-     * <p>
-     * By default, the execution is not skipped (set to {@code false}).
-     * <p>
-     * Example:
-     * <pre>{@code
-     * <skip>false</skip>
-     * }</pre>
-     */
-    @Parameter(defaultValue = "false")
-    boolean skip;
-    
-    /**
-     * Configuration used to indicate to the plugin about the root directory of the {@code git} repository we want to
-     * check. By default, uses {@code ${project.basedir}/.git}. Should work for most single-module projects. For Maven
-     * modules, use `../` to get higher up in the directory tree (for example {@code ${project.basedir}/../.git}).
-     * <p>
-     * Example:
-     * <pre>{@code
-     * <dotGitDirectory>
-     *   ${project.basedir}/.git
-     * </dotGitDirectory>
-     * }</pre>
-     */
-    @Parameter(defaultValue = "${project.basedir}/.git")
-    File dotGitDirectory;
+public class GitSubmoduleUpdateMojo extends GitSubmoduleMojo {
 
     @Override
-    public void execute() throws MojoExecutionException {
-        if (skip) {
-            getLog().info("Skipping execution of plugin.");
-            return;
-        }
-
-        if (!dotGitDirectory.exists()) {
-            String parent = dotGitDirectory.getParent();
-            getLog().info("No .git directory found at: " + parent + ". Skipping execution of plugin.");
-            return;
-        }
-
-        try (Git git = Git.open(dotGitDirectory)) {
-            Repository repository = git.getRepository();
-            git.submoduleInit().call();
-            git.submoduleUpdate().call();
-            traverse(repository);
-        } catch (GitAPIException | IOException ex) {
-            throw new MojoExecutionException(ex.getMessage(), ex);
-        }
+    public void execute(Git git) throws Exception {
+        Repository repository = git.getRepository();
+        git.submoduleInit().call();
+        git.submoduleUpdate().call();
+        traverse(repository);
     }
 
     private void traverse(Repository root) throws GitAPIException, IOException {
