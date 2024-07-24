fb4f5b688f9143de6049fcf9665929e70729c197
dabico <dabico@usi.ch>
2024-07-24 14:18:39 +0200

    Introduce abstraction for plugin Mojos

---
+++ src/main/java/ch/usi/si/seart/maven/GitSubmoduleMojo.java
@@ -0,0 +1,62 @@
+package ch.usi.si.seart.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.eclipse.jgit.api.Git;
+
+import java.io.File;
+
+abstract class GitSubmoduleMojo extends AbstractMojo {
+
+    /**
+     * When set to {@code true} the plugin execution will completely skip. This is useful for profile-activated plugin
+     * invocations or to use properties to enable/disable pom features.
+     * <p>
+     * By default, the execution is not skipped (set to {@code false}).
+     * <p>
+     * Example:
+     * <pre>{@code
+     * <skip>false</skip>
+     * }</pre>
+     */
+    @Parameter(defaultValue = "false")
+    boolean skip;
+
+    /**
+     * Configuration used to indicate to the plugin about the root directory of the {@code git} repository we want to
+     * check. By default, uses {@code ${project.basedir}/.git}. Should work for most single-module projects. For Maven
+     * modules, use `../` to get higher up in the directory tree (for example {@code ${project.basedir}/../.git}).
+     * <p>
+     * Example:
+     * <pre>{@code
+     * <dotGitDirectory>
+     *   ${project.basedir}/.git
+     * </dotGitDirectory>
+     * }</pre>
+     */
+    @Parameter(defaultValue = "${project.basedir}/.git")
+    File dotGitDirectory;
+
+    @Override
+    public final void execute() throws MojoExecutionException {
+        if (skip) {
+            getLog().info("Skipping execution of plugin.");
+            return;
+        }
+
+        if (!dotGitDirectory.exists()) {
+            String parent = dotGitDirectory.getParent();
+            getLog().info("No .git directory found at: " + parent + ". Skipping execution of plugin.");
+            return;
+        }
+
+        try (Git git = Git.open(dotGitDirectory)) {
+            execute(git);
+        } catch (Exception ex) {
+            throw new MojoExecutionException(ex.getMessage(), ex);
+        }
+    }
+
+    protected abstract void execute(Git git) throws Exception;
+}
